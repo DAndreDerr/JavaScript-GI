1d2e6fb3cfa32134bb72aac791e1f19ac34b9b0d
DAndreDerr <dandre020727@gmail.com>
2023-04-18 20:52:13 -0400

    first commit

---
+++ easy.js
@@ -0,0 +1,10 @@
+// Create two variables and assign a person’s name to each. 
+// Console log a statement that says which name is shorter or longer, and by how many characters. 
+
+let name1 = "Spongbob SquarePants"
+let name2 = "Patrick Star"
+let namediff= name1.length - name2.length;
+
+console.log("The name " + name1 + " is longer than " + name2+ " by " + namediff +" characters");
+
+// output is The name Spongbob SquarePants is longer than Patrick Star by 8 characters
---
+++ veryeasy.js
@@ -0,0 +1,8 @@
+// Create two variables and assign a number to each.
+// Console log the difference between the numbers.
+
+let num1 = 27
+let num2 = 7
+
+console.log(num1 - num2);
+// output is 20
