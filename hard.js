57c69c715701e7d8594e5bc31b6637e560f3a897
DAndreDerr <dandre020727@gmail.com>
2023-04-19 09:40:11 -0400

    finished hard challenge

---
+++ hard.js
@@ -0,0 +1,29 @@
+//  Create 4 math functions
+// one called add() that adds 2 numbers
+// one called subtract() that subtracts 2 numbers
+// one called multiply() to multiply 2 numbers
+// one called divide() to divide two numbers.
+
+let num1 = 20
+let num2 = 5
+
+function addition(n1,n2){
+    return n1 + n2;
+}
+
+function subtraction(n1,n2){
+    return n1 - n2;
+}
+
+function multiplication(n1,n2){
+    return n1 * n2;
+}
+
+function division(n1,n2){
+    return n1 / n2;
+}
+
+console.log(addition(num1,num2));
+console.log(subtraction(num1,num2));
+console.log(multiplication(num1,num2));
+console.log(division(num1,num2));
